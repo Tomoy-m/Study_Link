6feeb394e4bdfce178fa7dd3a9b3f4576dfe452d
Tomoy-m <tomoya.1215@outlook.ja>
2023-10-27 12:50:21 +0900

    CSSの書き換え 微修正

--- app/javascript/time.js
+++ app/javascript/time.js
@@ -1,9 +1,18 @@
-function showClock() {
-  let nowTime = new Date();
-  let nowHour = nowTime.getHours();
-  let nowMin  = nowTime.getMinutes();
-  let nowSec  = nowTime.getSeconds();
-  let msg = "現在時刻：" + nowHour + ":" + nowMin + ":" + nowSec;
-  document.getElementById("realtime").innerHTML = msg;
+$(document).on('turbolinks:load', function() {
+function set2fig(num) {
+  // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+  var ret;
+  if( num < 10 ) { ret = "0" + num; }
+  else { ret = num; }
+  return ret;
 }
-setInterval('showClock()',1000);
+function showClock2() {
+  var nowTime = new Date();
+  var nowHour = set2fig( nowTime.getHours() );
+  var nowMin  = set2fig( nowTime.getMinutes() );
+  var nowSec  = set2fig( nowTime.getSeconds() );
+  var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
+  document.getElementById("RealtimeClockArea2").innerHTML = msg;
+}
+setInterval('showClock2()',1000);
+});
